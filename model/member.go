984848be8e664c192f4bfc5fc43258656ddf597b
huangdongcheng <934313141@qq.com>
2021-01-11 17:26:11 +0800

    add member model

--- model/member.go
+++ model/member.go
@@ -3,7 +3,7 @@ package model
 type Member struct {
 	IdModel
 	Name      string `json:"name"`
-	Password  string `json:"name"`
+	Password  string `json:"password"`
 	Nickname  string `json:"nickname"`
 	Email     string `json:"email"`
 	Telephone string `json:"telephone"`
@@ -13,9 +13,6 @@ type Member struct {
 	TimeModel
 }
 
-
 func (Member) TableName() string {
 	return "member"
 }
-
-fun
