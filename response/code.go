7bb86efb2a1f8765d470151723fb1ff3c26544c9
huangdongcheng <rocky114!QAZ>
2021-06-29 11:19:06 +0800

    add go:generate command

--- response/code.go
+++ response/code.go
@@ -1,12 +1,16 @@
+//go:generate stringer -type ErrCode -linecomment
+
 package response
 
-const (
-	Success               = 0
-	Error                 = -1
-	RequestParamError     = 10000
-	CaptchaGenerateFailed = 10001
+type ErrCode int
 
-	MemberHasExist           = 20001
-	MemberCreateFailed       = 20002
-	MemberAccountPasswordErr = 20003
+const (
+	Success               ErrCode = 0
+	Error                 ErrCode = -1
+	RequestParamError     ErrCode = 10000
+	CaptchaGenerateFailed ErrCode = 10001
+
+	MemberHasExist           ErrCode = 20001
+	MemberCreateFailed       ErrCode = 20002
+	MemberAccountPasswordErr ErrCode = 20003
 )
