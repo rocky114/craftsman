c1dd4db27dcc734746e53b5a5b3055d9525ab6b1
rocky114 <934313141@qq.com>
2023-06-09 23:05:47 +0800

    crawler

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -11,6 +11,6 @@ import (
 func main() {
 
 	ctx := context.Background()
-	fmt.Println(crawler.Crawl(ctx, "4132010299", "2022"))
+	fmt.Println(crawler.Crawl(ctx, "4132010300", "2022"))
 
 }
---
+++ internal/crawler/nanjingxinxigongcheng_university.go
@@ -0,0 +1,83 @@
+package crawler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/rocky114/craftsman/internal/storage"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/rocky114/craftsman/internal/pkg/path"
+
+	"github.com/gocolly/colly/v2"
+)
+
+type nanjingxinxigongchengUniversity struct {
+	university
+}
+
+func init() {
+	collection["4132010300"] = &nanjingxinxigongchengUniversity{university{
+		name: "南京信息工程大学",
+		code: "4132010300",
+	}}
+}
+
+func (u *nanjingxinxigongchengUniversity) crawl(ctx context.Context) error {
+	c := colly.NewCollector(colly.CacheDir(path.GetTmpPath()))
+
+	detailCollector := c.Clone()
+
+	detailCollector.OnHTML(`div.con_wrap table.seaech_list`, func(element *colly.HTMLElement) {
+		element.ForEach("tr", func(i int, element *colly.HTMLElement) {
+			if i == 0 {
+				return
+			}
+
+			admissionTime := element.ChildText("td:nth-of-type(2)")
+			province := element.ChildText("td:nth-of-type(3)")
+			college := element.ChildText("td:nth-of-type(4)")
+			major := element.ChildText("td:nth-of-type(5)")
+			selectExam := element.ChildText("td:nth-of-type(6)")
+			admissionType := element.ChildText("td:nth-of-type(7)")
+			admissionNumber := element.ChildText("td:nth-of-type(8)")
+			provinceControlScoreLine := element.ChildText("td:nth-of-type(9)")
+			minScore := element.ChildText("td:nth-of-type(10)")
+			maxScore := element.ChildText("td:nth-of-type(11)")
+			averageScore := element.ChildText("td:nth-of-type(12)")
+
+			//fmt.Println(admissionTime, province, college, major, selectExam, admissionType, admissionNumber, provinceControlScoreLine, minScore, maxScore, averageScore)
+
+			if err := storage.GetQueries().CreateAdmissionMajor(ctx, storage.CreateAdmissionMajorParams{
+				University:               u.name,
+				College:                  college,
+				Province:                 province,
+				Major:                    major,
+				AdmissionType:            admissionType,
+				SelectExam:               selectExam,
+				AdmissionTime:            admissionTime,
+				AdmissionNumber:          admissionNumber,
+				ProvinceControlScoreLine: provinceControlScoreLine,
+				MinScore:                 minScore,
+				MaxScore:                 maxScore,
+				AverageScore:             averageScore,
+			}); err != nil {
+				logrus.Errorf("create admission major err: %v", err)
+			}
+		})
+	})
+
+	c.OnResponse(func(response *colly.Response) {
+		if response.StatusCode != http.StatusOK {
+			logrus.Errorf("%s university http code: %d", u.name, response.StatusCode)
+			return
+		}
+
+		if err := detailCollector.Post("https://zs.nuist.edu.cn/wnfs.jsp?wbtreeid=1011", map[string]string{"nf": u.admissionTime}); err != nil {
+			logrus.Errorf("nanjingxinxigongchengUniversity err: %v", err)
+		}
+	})
+
+	return c.Visit("https://zs.nuist.edu.cn/wnfs.jsp?wbtreeid=1011")
+}
